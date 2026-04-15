1221bc196eead179ec943a5baf4b0f54d6c70f4a
krishnagediya5 <krishnagediya5@gmail.com>
2026-04-15 21:37:45 +0530

    Update k.py

--- k.py
+++ k.py
@@ -636,9 +636,9 @@ else:
     # UNSUPERVISED
     # =========================
 
-else:
+        else:
     
-        st.subheader("🧠 Unsupervised Model Leaderboard")
+            st.subheader("🧠 Unsupervised Model Leaderboard")
     
             data = df.select_dtypes(include=np.number)
     
